8a4b0ece53654e4157524542d60704c07ba88481
zbf <zbfcqtl@gmail.com>
2023-02-07 16:15:38 +0800

    refactor: update builder and structure

--- build/index.js
+++ build/index.js
@@ -3,30 +3,32 @@ const fs = require('fs');
 const path = require('path');
 const { removeDir } = require('./u');
 
-const themeEntryList = fs.readdirSync(path.resolve(__dirname, '../themes'));
+const themeFolder = '../src/themes';
 
+// 获取到所有的主题文件夹
+const themeEntryList = fs.readdirSync(path.resolve(__dirname, themeFolder));
+
+// 处理生产环境目录
 const targetDir = path.resolve(__dirname, '../css');
 if (fs.existsSync(targetDir)) {
   removeDir(targetDir);
 }
 
 themeEntryList.forEach((themeName) => {
-  if (
-    fs.lstatSync(path.resolve(__dirname, `../themes/${themeName}`)).isFile()
-  ) {
+  // 目标主题目录不是目录是文件，退出
+  if (fs.lstatSync(path.resolve(__dirname, `${themeFolder}/${themeName}`)).isFile()) {
     return false;
   }
 
   const scssCode = fs.readFileSync(
-    path.resolve(__dirname, `../themes/${themeName}/index.scss`)
+    path.resolve(__dirname, `${themeFolder}/${themeName}/index.scss`)
   );
 
-  const result = sass.renderSync({
-    data: `@import "themes/common.scss";@import "themes/public.scss";${scssCode}`,
-    includePaths: [path.resolve(__dirname, '../')],
+  const result = sass.compile(path.resolve(__dirname, `${themeFolder}/${themeName}/index.scss`), {
+    charset: false,
+    style: 'compressed'
   });
 
-  //
   if (!fs.existsSync(targetDir)) {
     fs.mkdirSync(targetDir);
   }
