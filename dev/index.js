85b7f0bb9cf7c2f4e51c70d99af09aa53108ef84
zbf <zbfcqtl@gmail.com>
2024-08-05 11:24:48 +0800

    fix: the task list in the cyanosis theme is always checked

--- dev/index.js
+++ dev/index.js
@@ -1,4 +1,24 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
+import { config } from 'md-editor-v3';
+
+config({
+  markdownItPlugins(plugins, { editorId }) {
+    return plugins.map((item) => {
+      if (item.type === 'taskList') {
+        return {
+          ...item,
+          options: {
+            ...item.options,
+            enabled: true
+          }
+        };
+      }
+
+      return item;
+    });
+  }
+});
+
 createApp(App).mount('#app');
